747e4ac531d8f2d9052ed81ec141fbc06cb7c051
juan <maxdarkx@gmail.com>
2022-02-01 19:32:43 -0500

    se resuelve el problema nivel bonus mediante la funcion minimoComunMultiploAvanzado(num1, num2) = (num1 * num2)/maximoComunDivisor(num1, num2)

--- src/main/java/com/juancarlosmaya/testproject1/EjercicioMentoria.java
+++ src/main/java/com/juancarlosmaya/testproject1/EjercicioMentoria.java
@@ -33,21 +33,42 @@ public class EjercicioMentoria {
         return resultado;
     }
 
-    public static int funcion2(int a, int b)
+    public static int maximoComunDivisor(int numero1, int numero2)
     {
         //FIXME: Actualizar a y b de acuerdo a quien es mayor o menor, para que el algoritmo funcione
         int resultado = 0;
+        int temp;
+
+        if(numero2 > numero1)
+        {
+            temp = numero2;
+            numero2 = numero1;
+            numero1 = temp;
+        }
+
         do {
-            resultado = b;
-            b = a % b;
-            a = resultado;
-        } while (b != 0);
+            resultado = numero2;
+            numero2 = numero1 % numero2;
+            numero1 = resultado;
+        } while (numero2 != 0);
         return resultado;
     }
 
+    public static int minimoComunMultiploAvanzado(int numero1, int numero2)
+    {
+        return numero1 * numero2/maximoComunDivisor(numero1, numero2);
+    }
+
     public static void main(String[] args) {
-        int resultado = minimoComunMultiplo(3, 9);
-        System.out.println("El resultado es: "+resultado);
+        int numero1= 3;
+        int numero2 = 9;
+        int resultado = minimoComunMultiplo(numero1, numero2);
+        int resultado2 = maximoComunDivisor(numero1,numero2);
+        int resultado3 = minimoComunMultiploAvanzado(numero1, numero2);
+
+        System.out.println("El minimo comun multiplo es: "+resultado);
+        System.out.println("El maximo comun divisor es: "+resultado2);
+        System.out.println("El minimo comun multiplo (Avanzado) es: "+resultado3);
 
     }
 }
